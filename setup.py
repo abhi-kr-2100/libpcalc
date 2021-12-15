f8237484a4300c8c19519f72d545f5a70678a381
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 18:03:27 +0530

    Update version to 1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ pcalc_extension = Extension(
 
 setup(
     name='libpcalc',
-    version='1.0.0',
+    version='1.1.0',
     
     description='Evaluate Power Calculator expressions from Python',
     long_description=open('README.md').read(),
